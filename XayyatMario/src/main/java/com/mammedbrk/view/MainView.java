738e281cde7310775640898a09ecc9b722d95e80
mammedbrk <mammedbrk@gmail.com>
2023-04-01 00:27:37 +0330

    Create MainView and implement method in LoginView

--- XayyatMario/src/main/java/com/mammedbrk/view/MainView.java
+++ XayyatMario/src/main/java/com/mammedbrk/view/MainView.java
@@ -1,2 +1,16 @@
-package com.mammedbrk.view;public class MainView {
+package com.mammedbrk.view;
+
+import com.mammedbrk.listener.LoginFormListener;
+import com.mammedbrk.view.auth.LoginView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class MainView extends BorderPane {
+    public MainView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com.mammedbrk/fxml/auth/login-view.fxml"));
+        setCenter(loader.load());
+        ((LoginView) loader.getController()).setLoginFormListener(new LoginFormListener());
+    }
 }
--- XayyatMario/src/main/java/com/mammedbrk/view/auth/LoginView.java
+++ XayyatMario/src/main/java/com/mammedbrk/view/auth/LoginView.java
@@ -1,13 +1,46 @@
 package com.mammedbrk.view.auth;
 
+import com.mammedbrk.event.FormEvent;
+import com.mammedbrk.listener.LoginFormListener;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class LoginView {
-    public void signInClicked(ActionEvent e) {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    private LoginFormListener loginFormListener;
 
+
+    public void signInClicked(ActionEvent e) {
+        FormEvent formEvent = new FormEvent(this, getUsername(), getPassword());
+        System.out.println(loginFormListener.listen(formEvent));
     }
 
     public void signUpClicked(ActionEvent e) {
 
+    }
+
+    // Getters and setters
+
+    public LoginFormListener getLoginFormListener() {
+        return loginFormListener;
+    }
+
+    public void setLoginFormListener(LoginFormListener loginFormListener) {
+        this.loginFormListener = loginFormListener;
+    }
+
+    public String getUsername() {
+        return username.getText();
+    }
+
+    public String getPassword() {
+        return password.getText();
     }
 }
