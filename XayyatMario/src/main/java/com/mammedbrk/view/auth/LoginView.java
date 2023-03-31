708d935af51dded561a9e4217768958c9013a29a
mammedbrk <mammedbrk@gmail.com>
2023-03-31 22:19:23 +0330

    Create login page fxml and class, and add auth.css file

---
+++ XayyatMario/src/main/java/com/mammedbrk/view/auth/LoginView.java
@@ -0,0 +1,13 @@
+package com.mammedbrk.view.auth;
+
+import javafx.event.ActionEvent;
+
+public class LoginView {
+    public void signInClicked(ActionEvent e) {
+
+    }
+
+    public void signUpClicked(ActionEvent e) {
+
+    }
+}
