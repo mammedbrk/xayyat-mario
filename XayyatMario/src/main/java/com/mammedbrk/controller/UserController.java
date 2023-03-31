ed09c4cbf1ad886167bf35bdb363fff95cd131f0
mammedbrk <mammedbrk@gmail.com>
2023-03-31 20:49:28 +0330

    Create FormEvent

--- XayyatMario/src/main/java/com/mammedbrk/controller/UserController.java
+++ XayyatMario/src/main/java/com/mammedbrk/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.mammedbrk.controller;
 
+import com.mammedbrk.access.UserAccess;
+import com.mammedbrk.event.FormEvent;
+import com.mammedbrk.model.User;
+
 public class UserController {
-    
+    UserAccess userAccess = new UserAccess();
+
+    public boolean register(FormEvent e) {
+        return false;
+    }
 }
