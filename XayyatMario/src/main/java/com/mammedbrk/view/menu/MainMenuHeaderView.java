ae1c02505aa6a0b2fefebb40f68ec5bd074bbe32
mammedbrk <mammedbrk@gmail.com>
2023-04-08 02:43:34 +0330

    Implement methods in MainMenuHeaderView

--- XayyatMario/src/main/java/com/mammedbrk/view/menu/MainMenuHeaderView.java
+++ XayyatMario/src/main/java/com/mammedbrk/view/menu/MainMenuHeaderView.java
@@ -1,13 +1,50 @@
 package com.mammedbrk.view.menu;
 
+import com.mammedbrk.current.Current;
 import com.mammedbrk.listener.Listener;
+import com.mammedbrk.model.Character;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
+import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class MainMenuHeaderView {
-    private List<Listener<String>> listeners;
+public class MainMenuHeaderView implements Initializable {
+    @FXML
+    private Label username;
+    @FXML
+    private Label score;
+    @FXML
+    private Label coin;
+    @FXML
+    private Pane profileBtn;
+    @FXML
+    private ImageView characterImg;
+    private List<Listener<String>> listeners = new LinkedList<>();
+
+    // Methods
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        username.setText(Current.user.getUsername());
+        score.setText(String.valueOf(Current.user.getMaxScore()));
+        coin.setText(String.valueOf(Current.user.getNumOfCoins()));
+        profileBtn.setPrefWidth(Current.user.getUsername().length() * 5 + 200);
+        for (Character character: Current.user.getCharacters()) {
+            if (character.isChosen) {
+                characterImg.setImage(character.getImageAddress);
+                break;
+            }
+        }
+    }
 
     public void addListener(Listener<String> listener) {
         listeners.add(listener);
