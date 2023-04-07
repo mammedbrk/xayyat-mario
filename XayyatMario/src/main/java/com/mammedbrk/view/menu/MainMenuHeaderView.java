149c3543e63396bf451b2403b5eca8f2c384c052
mammedbrk <mammedbrk@gmail.com>
2023-04-07 22:36:57 +0330

    Create main menu

---
+++ XayyatMario/src/main/java/com/mammedbrk/view/menu/MainMenuHeaderView.java
@@ -0,0 +1,14 @@
+package com.mammedbrk.view.menu;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class MainMenuHeaderView {
+    public void profileBtnClicked(MouseEvent mouseEvent) {
+        System.out.println("profile");
+    }
+
+    public void shopBtnClicked(ActionEvent event) {
+        System.out.println("shop");
+    }
+}
---
+++ XayyatMario/src/main/java/com/mammedbrk/view/menu/MainMenuView.java
@@ -0,0 +1,11 @@
+package com.mammedbrk.view.menu;
+
+import javafx.event.ActionEvent;
+
+public class MainMenuView {
+    public void startBtnClicked(ActionEvent event) {
+    }
+
+    public void continueBtnClicked(ActionEvent event) {
+    }
+}
