55637707eaa96bb2c82c7f76f5dc1d8f2d5e08e2
mammedbrk <mammedbrk@gmail.com>
2023-04-10 02:52:38 +0330

    Modify FormListeners

--- XayyatMario/src/main/java/com/mammedbrk/listener/LoginFormListener.java
+++ XayyatMario/src/main/java/com/mammedbrk/listener/LoginFormListener.java
@@ -1,7 +1,6 @@
 package com.mammedbrk.listener;
 
 import com.mammedbrk.controller.UserController;
-import com.mammedbrk.current.Current;
 import com.mammedbrk.event.FormEvent;
 
 public class LoginFormListener implements Listener<FormEvent> {
@@ -9,7 +8,6 @@ public class LoginFormListener implements Listener<FormEvent> {
 
     @Override
     public boolean listen(FormEvent e) {
-        Current.user = controller.login(e);
-        return Current.user != null;
+        return controller.login(e) != null;
     }
 }
