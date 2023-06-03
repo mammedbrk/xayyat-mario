1dc20422e296df25e7ebc9b1259741bc1ec71b61
mammedbrk <mammedbrk@gmail.com>
2023-06-03 14:40:54 +0330

    Create ItemType and change item field type in QuestionBlock

--- XayyatMario/src/main/java/com/mammedbrk/model/component/block/QuestionBlock.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/block/QuestionBlock.java
@@ -1,25 +1,25 @@
 package com.mammedbrk.model.component.block;
 
-import com.mammedbrk.model.component.item.Item;
+import com.mammedbrk.model.component.item.ItemType;
 
 public class QuestionBlock extends Block {
-    private Item item;
+    private ItemType item;
 
     public QuestionBlock() {
     }
 
-    public QuestionBlock(int x, int y, Item item) {
+    public QuestionBlock(int x, int y, ItemType item) {
         super(x, y);
         this.item = item;
     }
 
     // Getters and setters
 
-    public Item getItem() {
+    public ItemType getItem() {
         return item;
     }
 
-    public void setItem(Item item) {
+    public void setItem(ItemType item) {
         this.item = item;
     }
 }
---
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/item/ItemType.java
@@ -0,0 +1,8 @@
+package com.mammedbrk.model.component.item;
+
+public enum ItemType {
+    COIN,
+    FLOWER,
+    MUSHROOM,
+    STAR
+}
