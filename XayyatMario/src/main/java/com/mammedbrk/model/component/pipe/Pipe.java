54fc1f42ff4c73fd835196426e76181630409c67
mammedbrk <mammedbrk@gmail.com>
2023-06-03 12:51:46 +0330

    Add JsonSubTypes annotation in Pipe class

--- XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/Pipe.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/Pipe.java
@@ -1,8 +1,20 @@
 package com.mammedbrk.model.component.pipe;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.mammedbrk.model.component.Component;
-import com.mammedbrk.model.component.block.Block;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SimplePipe.class, name = "SIMPLE"),
+        @JsonSubTypes.Type(value = TeleSimplePipe.class, name = "TELE_SIMPLE"),
+        @JsonSubTypes.Type(value = PiranhaTrapPipe.class, name = "PIRANHA_TRAP"),
+        @JsonSubTypes.Type(value = TelePiranhaPipe.class, name = "TELE_PIRANHA")
+})
 public abstract class Pipe extends Component {
     public Pipe() {
     }
