a72cb5fded1af77020952b6b8e327866e21ad1f7
mammedbrk <mammedbrk@gmail.com>
2023-06-03 11:32:48 +0330

    Modify Enemy class

--- XayyatMario/src/main/java/com/mammedbrk/model/component/enemy/Enemy.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/enemy/Enemy.java
@@ -10,55 +10,23 @@ import com.mammedbrk.model.component.Component;
         property = "type"
 )
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = Grave.class, name = "grave")
 })
 public abstract class Enemy extends Component {
-    protected int xCurrent, yCurrent;
-    protected double xVelocity, yVelocity;
+    protected boolean alive;
 
     public Enemy() {
     }
 
-    public Enemy(int x, int y, String imageAddress, double xVelocity, double yVelocity) {
-        super(x, y, imageAddress);
-        this.xVelocity = xVelocity;
-        this.yVelocity = yVelocity;
-        this.xCurrent = x;
-        this.yCurrent = y;
+    public Enemy(int x, int y) {
+        super(x, y);
+        alive = true;
     }
 
-    public abstract void modifySpeed();
-
-
-    public double getxVelocity() {
-        return xVelocity;
+    public boolean isAlive() {
+        return alive;
     }
 
-    public void setxVelocity(double xVelocity) {
-        this.xVelocity = xVelocity;
-    }
-
-    public double getyVelocity() {
-        return yVelocity;
-    }
-
-    public void setyVelocity(double yVelocity) {
-        this.yVelocity = yVelocity;
-    }
-
-    public int getxCurrent() {
-        return xCurrent;
-    }
-
-    public void setxCurrent(int xCurrent) {
-        this.xCurrent = xCurrent;
-    }
-
-    public int getyCurrent() {
-        return yCurrent;
-    }
-
-    public void setyCurrent(int yCurrent) {
-        this.yCurrent = yCurrent;
+    public void setAlive(boolean alive) {
+        this.alive = alive;
     }
 }
