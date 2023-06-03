50c88259feba713ae3ae0873ca8ac010f0e7c411
mammedbrk <mammedbrk@gmail.com>
2023-06-03 11:37:11 +0330

    Create TelePipe

--- XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/TelePipe.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/TelePipe.java
@@ -1,2 +1,24 @@
-package com.mammedbrk.model.component.pipe;public class TelePipe {
+package com.mammedbrk.model.component.pipe;
+
+import com.mammedbrk.model.Section;
+
+public class TelePipe extends Pipe {
+    protected Section section;
+
+    public TelePipe() {
+    }
+
+    public TelePipe(Section section) {
+        this.section = section;
+    }
+
+    // Getters and setters
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
