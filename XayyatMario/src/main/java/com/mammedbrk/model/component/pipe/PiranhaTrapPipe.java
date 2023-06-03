7306f40c93b0179dfcda9b281e1df141da1d2261
mammedbrk <mammedbrk@gmail.com>
2023-06-03 12:12:10 +0330

    Create PiranhaTrapPipe class and implement it

--- XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/PiranhaTrapPipe.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/pipe/PiranhaTrapPipe.java
@@ -1,5 +1,45 @@
 package com.mammedbrk.model.component.pipe;
 
-public class DeceitPipe extends Pipe {
-    
+import com.mammedbrk.model.component.enemy.Piranha;
+
+public class PiranhaTrapPipe extends Pipe implements Piranha {
+    private boolean alive;
+    private boolean shown;
+    private int time;
+
+
+    @Override
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public void die() {
+        alive = false;
+    }
+
+    @Override
+    public boolean isShown() {
+        return shown;
+    }
+
+    @Override
+    public void changeVisibility() {
+        if (shown && time == 3) { // todo read from config file
+            shown = false;
+        }
+        else if (!shown && time == 2) { // todo read from config file
+            shown = true;
+        }
+    }
+
+    @Override
+    public void resetTime() {
+        time = 0;
+    }
+
+    @Override
+    public void increaseTime() {
+        time++;
+    }
 }
