229977fee5b1805b925389131a181fb2b5d1aae6
mammedbrk <mammedbrk@gmail.com>
2023-06-04 13:46:58 +0330

    Fix models in master

--- XayyatMario/src/main/java/com/mammedbrk/model/component/item/Item.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/component/item/Item.java
@@ -1,7 +1,21 @@
 package com.mammedbrk.model.component.item;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.mammedbrk.model.component.Component;
+import com.mammedbrk.model.component.block.*;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Coin.class, name = "COIN"),
+        @JsonSubTypes.Type(value = Star.class, name = "STAR"),
+        @JsonSubTypes.Type(value = Mushroom.class, name = "MUSHROOM"),
+        @JsonSubTypes.Type(value = Flower.class, name = "FLOWER")
+})
 public abstract class Item extends Component {
     public Item() {
     }
