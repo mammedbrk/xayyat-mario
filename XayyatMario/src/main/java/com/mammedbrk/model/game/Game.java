b2ffb16d8aacfcd20156fbaa98828668f59b1f6b
mammedbrk <mammedbrk@gmail.com>
2023-06-05 11:59:20 +0330

    Create and implement game models

--- XayyatMario/src/main/java/com/mammedbrk/model/game/Game.java
+++ XayyatMario/src/main/java/com/mammedbrk/model/game/Game.java
@@ -1,11 +1,103 @@
-package com.mammedbrk.model;
+package com.mammedbrk.model.game;
 
 
+import com.mammedbrk.model.component.Mario;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
     private List<Level> levels;
     private int hearts;
     private int marioState;
+    private Mario mario;
+    private int score;
+    private int coins;
+    private int levelNo;
+    private int sectionNo;
 
+    public Game() {
+        levels = new ArrayList<>();
+        hearts = 3; // todo
+        marioState = 0;
+        mario = new Mario();
+        score = 0;
+        coins = 0;
+        levelNo = 0;
+        sectionNo = 0;
+    }
+
+    public Game(List<Level> levels, int hearts, int marioState) {
+        this.levels = levels;
+        this.hearts = hearts;
+        this.marioState = marioState;
+        mario = new Mario();
+        score = 0;
+        coins = 0;
+        levelNo = 0;
+        sectionNo = 0;
+    }
+
+    public Game(List<Level> levels, int hearts, int marioState, Mario mario, int score, int coins, int levelNo, int sectionNo) {
+        this.levels = levels;
+        this.hearts = hearts;
+        this.marioState = marioState;
+        this.mario = mario;
+        this.score = score;
+        this.coins = coins;
+        this.levelNo = levelNo;
+        this.sectionNo = sectionNo;
+    }
+
+    public void addLevel(Level level) {
+        levels.add(level);
+    }
+
+    public List<Level> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(List<Level> levels) {
+        this.levels = levels;
+    }
+
+    public int getHearts() {
+        return hearts;
+    }
+
+    public void setHearts(int hearts) {
+        this.hearts = hearts;
+    }
+
+    public int getMarioState() {
+        return marioState;
+    }
+
+    public void setMarioState(int marioState) {
+        this.marioState = marioState;
+    }
+
+    public Mario getMario() {
+        return mario;
+    }
+
+    public void setMario(Mario mario) {
+        this.mario = mario;
+    }
+
+    public int getLevelNo() {
+        return levelNo;
+    }
+
+    public void setLevelNo(int levelNo) {
+        this.levelNo = levelNo;
+    }
+
+    public int getSectionNo() {
+        return sectionNo;
+    }
+
+    public void setSectionNo(int sectionNo) {
+        this.sectionNo = sectionNo;
+    }
 }
