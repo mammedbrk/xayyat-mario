800fa5999d67d1752da37888915f606c1a2da4ff
mammedbrk <mammedbrk@gmail.com>
2023-06-08 14:40:27 +0330

    Modify method checkCollision in CollisionSystem

--- XayyatMario/src/main/java/com/mammedbrk/controller/CollisionSystem.java
+++ XayyatMario/src/main/java/com/mammedbrk/controller/CollisionSystem.java
@@ -6,14 +6,15 @@ public class CollisionSystem {
     public CollisionDirection checkCollision(Component a, Component b) {
         double dy = a.getY() - b.getY();
         double dx = a.getX() - b.getX();
-        
-        if (Math.abs(dy) < 1. && Math.abs(dx) < 1.) {
-            if (Math.abs(dx) <= Math.abs(dy)) {
+        double error = .00001;
+
+        if (Math.abs(dy) < 1. - error && Math.abs(dx) < 1. - error) {
+            if (Math.abs(dy) > .5 && Math.abs(dy) > Math.abs(dx)) {
                 if (dy > 0) return CollisionDirection.bottom_to_top;
-                else return CollisionDirection.top_to_bottom;
-            } else {
+                else if (dy < 0) return CollisionDirection.top_to_bottom;
+            } else if (Math.abs(dx) > error){
                 if (dx > 0) return CollisionDirection.left_to_right;
-                else return CollisionDirection.right_to_left;
+                else if (dx < 0) return CollisionDirection.right_to_left;
             }
         }
         return CollisionDirection.none;
