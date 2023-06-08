cde53ea83c29952410a50cb04ef9735ad3c5e8d5
mammedbrk <mammedbrk@gmail.com>
2023-06-09 01:28:56 +0330

    Implement BlockCollisionVisitor

--- XayyatMario/src/main/java/com/mammedbrk/controller/collision/BlockCollisionVisitor.java
+++ XayyatMario/src/main/java/com/mammedbrk/controller/collision/BlockCollisionVisitor.java
@@ -8,34 +8,99 @@ import com.mammedbrk.model.component.item.Item;
 import com.mammedbrk.model.interfaces.Changeable;
 import com.mammedbrk.model.interfaces.Gravitational;
 import com.mammedbrk.model.interfaces.Hittable;
+import com.mammedbrk.model.interfaces.Movable;
 
-public class BlockCollisionVisitor implements CollisionVisitor<Block> {
-    private CollisionSystem collisionSystem = new CollisionSystem();
+import java.util.List;
+
+public class BlockCollisionVisitor implements CollisionVisitor {
+    private final CollisionSystem collisionSystem = new CollisionSystem();
+    private List<Block> blocks;
+
     @Override
-    public void visit(Block block, Mario mario) {
-        switch (collisionSystem.checkCollision(block, mario)) {
-            case top_to_bottom -> {
-                mario.setY(block.getY() - 1);
-                mario.resetGravity();
-                if (block instanceof Hittable)
-                    ((Hittable) block).hit();
+    public void visit(Mario mario) {
+        for (Block block: blocks) {
+            switch (collisionSystem.checkCollision(mario, block)) {
+                case top_to_bottom -> {
+                    mario.setY(block.getY() - 1);
+                    mario.resetGravity();
+                    if (block instanceof Hittable)
+                        ((Hittable) block).hit();
+                }
+                case bottom_to_top -> {
+                    mario.setY(block.getY() + 1.);
+                    mario.resetGravity();
+                }
+                case right_to_left -> mario.setX(block.getX() - 1);
+                case left_to_right -> mario.setX(block.getX() + 1);
             }
-            case bottom_to_top -> {
-                mario.setY(block.getY() + 1.);
-                mario.resetGravity();
-            }
-            case right_to_left -> mario.setX(block.getX() - 1);
-            case left_to_right -> mario.setX(block.getX() + 1);
         }
     }
 
     @Override
-    public void visit(Block block, Enemy enemy) {
-
+    public void visit(Enemy enemy) {
+        for (Block block: blocks) {
+            switch (collisionSystem.checkCollision(enemy, block)) {
+                case bottom_to_top -> {
+                    if (enemy instanceof Gravitational) {
+                        enemy.setY(block.getY() + 1.);
+                        ((Gravitational) enemy).resetGravity();
+                    }
+                }
+                case left_to_right -> {
+                    if (enemy instanceof Movable) {
+                        ((Movable) enemy).changeDirection();
+                        enemy.setX(block.getX() + 1.);
+                    }
+                }
+                case right_to_left -> {
+                    if (enemy instanceof Movable) {
+                        ((Movable) enemy).changeDirection();
+                        enemy.setX(block.getX() - 1.);
+                    }
+                }
+                case top_to_bottom -> {
+                    if (enemy instanceof Gravitational) {
+                        enemy.setY(block.getY() - 1.);
+                        ((Gravitational) enemy).resetGravity();
+                    }
+                }
+            }
+        }
     }
 
     @Override
-    public void visit(Block block, Item item) {
+    public void visit(Item item) {
+        for (Block block: blocks) {
+            switch (collisionSystem.checkCollision(item, block)) {
+                case bottom_to_top -> {
+                    if (item instanceof Gravitational) {
+                        item.setY(block.getY() + 1.);
+                        ((Gravitational) item).resetGravity();
+                    }
+                }
+                case left_to_right -> {
+                    if (item instanceof Movable) {
+                        ((Movable) item).changeDirection();
+                        item.setX(block.getX() + 1.);
+                    }
+                }
+                case right_to_left -> {
+                    if (item instanceof Movable) {
+                        ((Movable) item).changeDirection();
+                        item.setX(block.getX() - 1.);
+                    }
+                }
+                case top_to_bottom -> {
+                    if (item instanceof Gravitational) {
+                        item.setY(block.getY() - 1.);
+                        ((Gravitational) item).resetGravity();
+                    }
+                }
+            }
+        }
+    }
 
+    public void setBlocks(List<Block> blocks) {
+        this.blocks = blocks;
     }
 }
--- XayyatMario/src/main/java/com/mammedbrk/controller/collision/CollisionVisitor.java
+++ XayyatMario/src/main/java/com/mammedbrk/controller/collision/CollisionVisitor.java
@@ -4,8 +4,8 @@ import com.mammedbrk.model.component.*;
 import com.mammedbrk.model.component.enemy.Enemy;
 import com.mammedbrk.model.component.item.Item;
 
-public interface CollisionVisitor<E> {
-    void visit(E e, Mario mario);
-    void visit(E e, Enemy enemy);
-    void visit(E e, Item item);
+public interface CollisionVisitor {
+    void visit(Mario mario);
+    void visit(Enemy enemy);
+    void visit(Item item);
 }
