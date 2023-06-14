4c8491a97a78d94d7755a9d0912ab0b5ad670d9b
mammedbrk <mammedbrk@gmail.com>
2023-06-14 10:01:01 +0330

    Modify NewGameSetupView to align with new modeling structure

--- XayyatMario/src/main/java/com/mammedbrk/view/game/NewGameSetupView.java
+++ XayyatMario/src/main/java/com/mammedbrk/view/game/NewGameSetupView.java
@@ -3,7 +3,7 @@ package com.mammedbrk.view.game;
 import com.mammedbrk.current.Current;
 import com.mammedbrk.listener.SaveNewGameListener;
 import com.mammedbrk.listener.StringListener;
-import com.mammedbrk.model.Game;
+import com.mammedbrk.model.game.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -33,74 +33,52 @@ public class NewGameSetupView implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (Current.user.getGames().get(0).getCurrentLevel() != null) {
+        if (!Current.user.getGames().get(0).getLevels().isEmpty()) {
             gameBtn1.setText(buttonText(Current.user.getGames().get(0)));
         }
-        if (Current.user.getGames().get(1).getCurrentLevel() != null) {
+        if (!Current.user.getGames().get(1).getLevels().isEmpty()) {
             gameBtn2.setText(buttonText(Current.user.getGames().get(1)));
         }
-        if (Current.user.getGames().get(2).getCurrentLevel() != null) {
+        if (!Current.user.getGames().get(2).getLevels().isEmpty()) {
             gameBtn3.setText(buttonText(Current.user.getGames().get(2)));
         }
     }
 
     private String buttonText(Game game) {
-        return "L" + game.getCurrentLevel().getNo() +
-                ", S" + game.getCurrentLevel().getCurrentSection().getNo() +
-                " | score: " + (game.getScore() + game.getCurrentLevel().getScore() + game.getCurrentLevel().getCurrentSection().getScore()) +
-                " | coins: " + (game.getCoins() + game.getCurrentLevel().getCoins() + game.getCurrentLevel().getCurrentSection().getCoins());
+        return "L" + game.getLevelNo() +
+                ", S" + game.getSectionNo() +
+                " | score: " + game.getScore() +
+                " | coins: " + game.getCoins();
     }
 
-    public void gameBtn1Clicked(ActionEvent event) {
-        if (Current.user.getGames().get(0).getCurrentLevel() != null) {
+    private void startNewGame(int index) {
+        if (!Current.user.getGames().get(index).getLevels().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Sure to delete previous game and save this on it?");
             if (alert.showAndWait().get() == ButtonType.OK) {
-                saveGameListener.listen(0);
+                saveGameListener.listen(index);
                 for (StringListener listener: listeners) {
                     listener.listen("StartGame");
                 }
             }
         }
         else {
-            saveGameListener.listen(0);
+            saveGameListener.listen(index);
             for (StringListener listener: listeners) {
                 listener.listen("StartGame");
             }
         }
+    }
+
+    public void gameBtn1Clicked(ActionEvent event) {
+        startNewGame(0);
     }
 
     public void gameBtn2Clicked(ActionEvent event) {
-        if (Current.user.getGames().get(1).getCurrentLevel() != null) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Sure to delete previous game and save this on it?");
-            if (alert.showAndWait().get() == ButtonType.OK) {
-                saveGameListener.listen(1);
-                for (StringListener listener: listeners) {
-                    listener.listen("StartGame");
-                }
-            }
-        } else {
-            saveGameListener.listen(1);
-            for (StringListener listener: listeners) {
-                listener.listen("StartGame");
-            }
-        }
+        startNewGame(1);
     }
 
     public void gameBtn3Clicked(ActionEvent event) {
-        if (Current.user.getGames().get(2).getCurrentLevel() != null) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Sure to delete previous game and save this on it?");
-            if (alert.showAndWait().get() == ButtonType.OK) {
-                saveGameListener.listen(2);
-                for (StringListener listener: listeners) {
-                    listener.listen("StartGame");
-                }
-            }
-        } else {
-            saveGameListener.listen(2);
-            for (StringListener listener: listeners) {
-                listener.listen("StartGame");
-            }
-        }
+        startNewGame(2);
     }
 
     public void backBtnClicked(ActionEvent event) {
